d2e7036376ab9a62df12b101fc03cd0e8914d8be
katao <kosgeika@gmail.com>
2023-08-03 13:36:30 +0200

    implementation of kafka consumer

--- src/main/java/com/kafkalearning/eventconsumer/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/kafkalearning/eventconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,25 +1,38 @@
 package com.kafkalearning.eventconsumer.config;
 
+import com.kafkalearning.eventconsumer.service.LibraryEventService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableKafka
 @RequiredArgsConstructor // enables constructor injection
+@Slf4j
 public class LibraryEventsConsumerConfig {
 
-    KafkaProperties properties;
+    KafkaProperties kafkaProperties;
     //Used when we want to manually control how we acknowledge messages consumption from a Kafka Topic.
+    private final LibraryEventService libraryEventsService;
 
     @Bean
     @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
@@ -28,8 +41,90 @@ public class LibraryEventsConsumerConfig {
             ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, kafkaConsumerFactory
-                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties())));
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.BATCH);
+                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
+        factory.setConcurrency(3);
+        // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setErrorHandler(((thrownException, data) -> {
+            log.info("Exception in consumerConfig is {} and the record is {}", thrownException.getMessage(), data);
+            //persist
+        }));
+        factory.setRetryTemplate(retryTemplate());
+        factory.setRecoveryCallback((context -> {
+            if (context.getLastThrowable().getCause() instanceof RecoverableDataAccessException) {
+                //invoke recovery logic
+                log.info("Inside the recoverable logic");
+                Arrays.asList(context.attributeNames())
+                        .forEach(attributeName -> {
+                            log.info("Attribute name is : {} ", attributeName);
+                            log.info("Attribute Value is : {} ", context.getAttribute(attributeName));
+                        });
+
+                ConsumerRecord<Integer, String> consumerRecord = (ConsumerRecord<Integer, String>) context.getAttribute("record");
+                libraryEventsService.handleRecovery(consumerRecord);
+            } else {
+                log.info("Inside the non recoverable logic");
+                throw new RuntimeException(context.getLastThrowable().getMessage());
+            }
+
+
+            return null;
+        }));
         return factory;
+    }
+
+   /* @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+        factory.setConcurrency(3);
+       // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setErrorHandler(((thrownException, data) -> {
+            log.info("Exception in consumerConfig is {} and the record is {}", thrownException.getMessage(), data);
+            //persist
+        }));
+        factory.setRetryTemplate(retryTemplate());
+        factory.setRecoveryCallback((context -> {
+            if(context.getLastThrowable().getCause() instanceof RecoverableDataAccessException){
+                //invoke recovery logic
+                log.info("Inside the recoverable logic");
+               Arrays.asList(context.attributeNames())
+                        .forEach(attributeName -> {
+                            log.info("Attribute name is : {} ", attributeName);
+                            log.info("Attribute Value is : {} ", context.getAttribute(attributeName));
+                        });
+
+               ConsumerRecord<Integer, String> consumerRecord = (ConsumerRecord<Integer, String>) context.getAttribute("record");
+                libraryEventsService.handleRecovery(consumerRecord);
+            }else{
+                log.info("Inside the non recoverable logic");
+                throw new RuntimeException(context.getLastThrowable().getMessage());
+            }
+
+
+            return null;
+        }));
+        return factory;
+    }*/
+
+    private RetryTemplate retryTemplate() {
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        return retryTemplate;
+    }
+
+    private RetryPolicy simpleRetryPolicy() {
+        /*SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);*/
+        Map<Class<? extends Throwable>, Boolean> exceptionsMap = new HashMap<>();
+        exceptionsMap.put(IllegalArgumentException.class, false);
+        exceptionsMap.put(RecoverableDataAccessException.class, true);
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy(3, exceptionsMap, true);
+        return simpleRetryPolicy;
     }
 }
--- src/main/java/com/kafkalearning/eventconsumer/service/LibraryEventService.java
+++ src/main/java/com/kafkalearning/eventconsumer/service/LibraryEventService.java
@@ -7,7 +7,14 @@ import com.kafkalearning.eventconsumer.reposity.LibraryEventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.dao.RecoverableDataAccessException;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -15,29 +22,79 @@ import org.springframework.stereotype.Service;
 public class LibraryEventService {
 
     private final ObjectMapper objectMapper;
-    private final LibraryEventRepository libraryEventRepository;
+    private final LibraryEventRepository libraryEventsRepository;
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
 
     public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
         LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
-        log.info("libraryEvent: {}", libraryEvent);
+        log.info("libraryEvent : {} ", libraryEvent);
+
+        if (libraryEvent.getLibraryEventId() != null && libraryEvent.getLibraryEventId() == 000) {
+            throw new RecoverableDataAccessException("Temporary Network Issue");
+        }
 
         switch (libraryEvent.getLibraryEventType()) {
-
             case NEW:
-                saveLibraryEvent(libraryEvent);
+                save(libraryEvent);
                 break;
             case UPDATE:
-
+                //validate the libraryevent
+                validate(libraryEvent);
+                save(libraryEvent);
                 break;
-
             default:
-                log.info("Invalid library event type");
+                log.info("Invalid Library Event Type");
+        }
+
+    }
+
+    private void validate(LibraryEvent libraryEvent) {
+        if (libraryEvent.getLibraryEventId() == null) {
+            throw new IllegalArgumentException("Library Event Id is missing");
+        }
+
+        Optional<LibraryEvent> libraryEventOptional = libraryEventsRepository.findById(libraryEvent.getLibraryEventId());
+        if (libraryEventOptional.isEmpty()) {
+            throw new IllegalArgumentException("Not a valid library Event");
+        }
+        log.info("Validation is successful for the library Event : {} ", libraryEventOptional.get());
+    }
+
+    private void save(LibraryEvent libraryEvent) {
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+        log.info("Successfully Persisted the libary Event {} ", libraryEvent);
+    }
+
+    public void handleRecovery(ConsumerRecord<Integer, String> record) {
+
+        Integer key = record.key();
+        String message = record.value();
+
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, message);
+        listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, message, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, message, result);
+            }
+        });
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error Sending the Message and the exception is {}", ex.getMessage());
+        try {
+            throw ex;
+        } catch (Throwable throwable) {
+            log.error("Error in OnFailure: {}", throwable.getMessage());
         }
     }
 
-    private void saveLibraryEvent(LibraryEvent libraryEvent) {
-        libraryEvent.getBook().setLibraryEvent(libraryEvent);
-        libraryEventRepository.save(libraryEvent);
-        log.info("Successfully persisted library event payload");
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message Sent SuccessFully for the key : {} and the value is {} , partition is {}", key, value, result.getRecordMetadata().partition());
     }
 }
